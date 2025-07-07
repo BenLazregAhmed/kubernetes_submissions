a5f109473d54d3d17af54013c6f67c1ddf5ba5ae
CRVX <benlazregahmed328@gmail.com>
2025-07-07 11:39:49 +0100

    todo app added

---
+++ todo/src/main/java/org/kubernetes/todo/TodoApplication.java
@@ -0,0 +1,22 @@
+package org.kubernetes.todo;
+
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class TodoApplication {
+    @Value("${server.port}")
+    private String port;
+    public static void main(String[] args) {
+        SpringApplication.run(TodoApplication.class, args);
+    }
+
+    @PostConstruct
+    public void onStartup() {
+        System.out.println("Server started in port " + port);
+    }
+}
