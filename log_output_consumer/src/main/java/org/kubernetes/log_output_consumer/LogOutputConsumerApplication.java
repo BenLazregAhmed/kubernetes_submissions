47f7217cbd7469445673adce931deca65b9c379e
CRVX <benlazregahmed328@gmail.com>
2025-07-24 17:10:46 +0100

    added log output consumer

---
+++ log_output_consumer/src/main/java/org/kubernetes/log_output_consumer/LogOutputConsumerApplication.java
@@ -0,0 +1,13 @@
+package org.kubernetes.log_output_consumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogOutputConsumerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LogOutputConsumerApplication.class, args);
+    }
+
+}
---
+++ log_output_consumer/src/test/java/org/kubernetes/log_output_consumer/LogOutputConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.kubernetes.log_output_consumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogOutputConsumerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
