6ac43c31f60231c3359e1004ca519f7c6c1ed96e
CRVX <benlazregahmed328@gmail.com>
2025-07-24 17:11:20 +0100

    finished log output producer and consumer

---
+++ log_output_consumer/src/main/java/org/kubernetes/log_output_consumer/LogConsumerRestController.java
@@ -0,0 +1,21 @@
+package org.kubernetes.log_output_consumer;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/logOutput")
+public class LogConsumerRestController {
+
+    private final LogReaderService logReaderService;
+
+    @GetMapping("/status")
+    public String getStatus() {
+        return logReaderService.readLastLine();
+    }
+}
+
